a4abd7465862875fb24040c40e33188cf17fba09
Kdolphin2284 <kdolphin2284@gmail.com>
2021-03-19 19:52:07 -0400

    Debug - Image Issue
    I fixed an issue where images were not loading.

--- js/main.js
+++ js/main.js
@@ -226,7 +226,7 @@ function userPicked(whichOne) {
       modalTextThreeObj.innerHTML = 'To find out which stock your birthday represents, click on the input fields at the top of the site, and fill in the boxes with your name and your birthday. When that’s done, press the submit button to view the cooresponding stock.';
       break;
     case 'googleLogo':
-      modalObj.style = "background: url('../media/images/googleImg.png') center no-repeat, linear-gradient(130deg, rgba(255,255,255,1) 0%, rgba(255,255,255,0.4674361861861862) 100%);";
+      modalObj.style = "background: url('./media/images/googleImg.png') center no-repeat, linear-gradient(130deg, rgba(255,255,255,1) 0%, rgba(255,255,255,0.4674361861861862) 100%);";
       modalHeadObj.innerHTML = 'Alphabet Inc. (GOOGL)';
       modalDateObj.innerHTML = 'January 20 - February 18';
       modalTextOneObj.innerHTML = 'Google, now known as a child company of Alphabet Inc., was founded September 4, 1998, in Menlo Park, California. It was founded by Larry Page and Sergey Brin, who were both students at Standford University in California.';
@@ -237,7 +237,7 @@ function userPicked(whichOne) {
       googleSoundObj.play();
       break;
     case 'teslaLogo':
-      modalObj.style = "background: url('../media/images/teslaImg.png') center no-repeat, linear-gradient(130deg, rgba(255,255,255,1) 0%, rgba(255,255,255,0.4674361861861862) 100%);";
+      modalObj.style = "background: url('./media/images/teslaImg.png') center no-repeat, linear-gradient(130deg, rgba(255,255,255,1) 0%, rgba(255,255,255,0.4674361861861862) 100%);";
       modalHeadObj.innerHTML = 'Tesla Inc. (TSLA)';
       modalDateObj.innerHTML = 'February 19 - March 20';
       modalTextOneObj.innerHTML = 'Tesla was founded July 1, 2003, in San Carlos, California. It was founded by Elon Musk, who is now currently the richest person in the world, with a net worth of $199.9 billion. ';
@@ -248,7 +248,7 @@ function userPicked(whichOne) {
       teslaSoundObj.play();
       break;  
     case 'jnjLogo':
-      modalObj.style = "background: url('../media/images/jnjImg.png') center no-repeat, linear-gradient(130deg, rgba(255,255,255,1) 0%, rgba(255,255,255,0.4674361861861862) 100%);";
+      modalObj.style = "background: url('./media/images/jnjImg.png') center no-repeat, linear-gradient(130deg, rgba(255,255,255,1) 0%, rgba(255,255,255,0.4674361861861862) 100%);";
       modalHeadObj.innerHTML = 'Johnson and Johnson (JNJ)';
       modalDateObj.innerHTML = 'March 21 - April 19';
       modalTextOneObj.innerHTML = 'Johnson and Johnson was founded January 1886, in New Brunswick, New Jersey. It was founded by James Wood Johnson, Edward Mead Johnson, and Robert Wood Johnson I.';
@@ -259,7 +259,7 @@ function userPicked(whichOne) {
       jnjSoundObj.play();
       break;
       case 'msftLogo':
-      modalObj.style = "background: url('../media/images/msftImg.png') center no-repeat, linear-gradient(130deg, rgba(255,255,255,1) 0%, rgba(255,255,255,0.4674361861861862) 100%);";
+      modalObj.style = "background: url('./media/images/msftImg.png') center no-repeat, linear-gradient(130deg, rgba(255,255,255,1) 0%, rgba(255,255,255,0.4674361861861862) 100%);";
       modalHeadObj.innerHTML = 'Microsoft Corp. (MSFT)';
       modalDateObj.innerHTML = 'April 20 - May 20';
       modalTextOneObj.innerHTML = 'Microsoft was founded April 4, 1975, in Albuquerque, New Mexico. It was founded by Bill Gates and Paul Allen, who were childhood friends with interests in topics like computer science.';
@@ -270,7 +270,7 @@ function userPicked(whichOne) {
       msftSoundObj.play();
       break;
       case 'appleLogo':
-      modalObj.style = "background: url('../media/images/appleImg.png') center no-repeat, linear-gradient(130deg, rgba(255,255,255,1) 0%, rgba(255,255,255,0.4674361861861862) 100%);";
+      modalObj.style = "background: url('./media/images/appleImg.png') center no-repeat, linear-gradient(130deg, rgba(255,255,255,1) 0%, rgba(255,255,255,0.4674361861861862) 100%);";
       modalHeadObj.innerHTML = 'Apple Inc. (AAPL)';
       modalDateObj.innerHTML = 'May 21 - June 20';
       modalTextOneObj.innerHTML = 'Apple was founded April 1, 1976, in Cupertino, California. It was founded by Steve Jobs, Steve Wozniak, and Ronald Wayne. Fun fact: Both Steve’s were college dropouts, and yet were extremely successful!';
@@ -281,7 +281,7 @@ function userPicked(whichOne) {
       appleSoundObj.play();
       break;
       case 'gmeLogo':
-      modalObj.style = "background: url('../media/images/gmeImg.png') center no-repeat, linear-gradient(130deg, rgba(255,255,255,1) 0%, rgba(255,255,255,0.4674361861861862) 100%);";
+      modalObj.style = "background: url('./media/images/gmeImg.png') center no-repeat, linear-gradient(130deg, rgba(255,255,255,1) 0%, rgba(255,255,255,0.4674361861861862) 100%);";
       modalHeadObj.innerHTML = 'Gamestop Corp. (GME)';
       modalDateObj.innerHTML = 'June 21 - July 22';
       modalTextOneObj.innerHTML = 'Gamestop was founded 1984 in Dallas, Texas, by James McCurry and Gary M. Kusin. It was formerly called Babbge’s, and was a software retailer before being renamed in December 2000.';
@@ -292,7 +292,7 @@ function userPicked(whichOne) {
       gmeSoundObj.play();
       break;
       case 'pfizerLogo':
-      modalObj.style = "background: url('../media/images/pfizerImg.png') center no-repeat, linear-gradient(130deg, rgba(255,255,255,1) 0%, rgba(255,255,255,0.4674361861861862) 100%);";
+      modalObj.style = "background: url('./media/images/pfizerImg.png') center no-repeat, linear-gradient(130deg, rgba(255,255,255,1) 0%, rgba(255,255,255,0.4674361861861862) 100%);";
       modalHeadObj.innerHTML = 'Pfizer Inc. (PFE)';
       modalDateObj.innerHTML = 'July 23 - August 22';
       modalTextOneObj.innerHTML = 'Pfizer was founded 1849 in Brooklyn, New York, New York. It was founded by Charles F. Erhart and Charles Pfizer, hence the name \“Pfizer.\"';
@@ -303,7 +303,7 @@ function userPicked(whichOne) {
       pfizerSoundObj.play();
       break;
       case 'amznLogo':
-      modalObj.style = "background: url('../media/images/amznImg.png') center no-repeat, linear-gradient(130deg, rgba(255,255,255,1) 0%, rgba(255,255,255,0.4674361861861862) 100%);";
+      modalObj.style = "background: url('./media/images/amznImg.png') center no-repeat, linear-gradient(130deg, rgba(255,255,255,1) 0%, rgba(255,255,255,0.4674361861861862) 100%);";
       modalHeadObj.innerHTML = 'Amazon.com Inc. (AMZN)';
       modalDateObj.innerHTML = 'August 23 - September 22';
       modalTextOneObj.innerHTML = 'Amazon was founded July 5, 1994, in Bellevue, Washington. It was founded by Jeff Bezos, who is the past \“richest person in the world\”, just recently surpassed by Elon Musk.';
@@ -314,7 +314,7 @@ function userPicked(whichOne) {
       amznSoundObj.play();
       break;
       case 'nflxLogo':
-      modalObj.style = "background: url('../media/images/nflxImg.png') center no-repeat, linear-gradient(130deg, rgba(255,255,255,1) 0%, rgba(255,255,255,0.4674361861861862) 100%);";
+      modalObj.style = "background: url('./media/images/nflxImg.png') center no-repeat, linear-gradient(130deg, rgba(255,255,255,1) 0%, rgba(255,255,255,0.4674361861861862) 100%);";
       modalHeadObj.innerHTML = 'Netflix Inc. NFLX';
       modalDateObj.innerHTML = 'September 23 - October 22';
       modalTextOneObj.innerHTML = 'Netflix was founded August 29. 1997, in Scotts Valley, California. It was founded by Reed Hastings and Marc Randolph, who both worked at Reed Hastings’ company called Pure Atria.';
@@ -325,7 +325,7 @@ function userPicked(whichOne) {
       nflxSoundObj.play();
       break;
       case 'nvdaLogo':
-      modalObj.style = "background: url('../media/images/nvdaImg.png') center no-repeat, linear-gradient(130deg, rgba(255,255,255,1) 0%, rgba(255,255,255,0.4674361861861862) 100%);";
+      modalObj.style = "background: url('./media/images/nvdaImg.png') center no-repeat, linear-gradient(130deg, rgba(255,255,255,1) 0%, rgba(255,255,255,0.4674361861861862) 100%);";
       modalHeadObj.innerHTML = 'NVIDIA Corp. (NVDA)';
       modalDateObj.innerHTML = 'October 23 - November 21';
       modalTextOneObj.innerHTML = 'NVIDIA was founded April 1993, in Santa Clara, California. It was founded by Jensen Huang, Chris Malachowsky, and Curtis Priem. Jensen Huang has been the CEO since 1993.';
@@ -336,7 +336,7 @@ function userPicked(whichOne) {
       nvdaSoundObj.play();
       break;
       case 'btcLogo':
-      modalObj.style = "background: url('../media/images/btcImg.png') center no-repeat, linear-gradient(130deg, rgba(255,255,255,1) 0%, rgba(255,255,255,0.4674361861861862) 100%);";
+      modalObj.style = "background: url('./media/images/btcImg.png') center no-repeat, linear-gradient(130deg, rgba(255,255,255,1) 0%, rgba(255,255,255,0.4674361861861862) 100%);";
       modalHeadObj.innerHTML = 'Bitcoin (BTC)';
       modalDateObj.innerHTML = 'November 22 - December 21';
       modalTextOneObj.innerHTML = 'Bitcoin was created January 2009. The “creator” goes by the name Satoshi Nakamoto, which is the presumed name of the person/people who made the currency.';
@@ -347,6 +347,7 @@ function userPicked(whichOne) {
       btcSoundObj.play();
       break;
       case 'etherLogo':
+      modalObj.style = "background: url('./media/images/etherImg.png') center no-repeat, linear-gradient(130deg, rgba(255,255,255,1) 0%, rgba(255,255,255,0.4674361861861862) 100%);";
       modalHeadObj.innerHTML = 'Ethereum (ETH)';
       modalDateObj.innerHTML = 'December 22 - January 19';
       modalTextOneObj.innerHTML = 'Ethereum was created July 30, 2015. It was founded by Vitalik Buterin, and financed by Gavin Wood, Charles Hoskinson, and Anthony Di Iorio.';
